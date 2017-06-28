263a78505920ead3918ca61255ad8b7c98755cd6
Liews Wuttipat <sweilz.w@gmail.com>
2017-06-28 14:02:49 +0700

    change main

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from node_class import NodePub,NodeSub
 
 
 def main():
-    microphone = NodeSub("tcp://127.0.0.1:5000","microphone")
+    microphone = NodeSub("tcp://192.168.1.244:5000","microphone")
     speaker = NodePub("tcp://127.0.0.1:4000")
 
     def play_microphone(data):
+        # print("Recieve data")
         speaker.send("speaker",data)
 
     microphone.run(play_microphone)
