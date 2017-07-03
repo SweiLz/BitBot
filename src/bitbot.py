02cd3f38af861116868e7dcfa3232aca5a69fadc
Liews Wuttipat <sweilz.w@gmail.com>
2017-07-03 19:16:08 +0700

    make class bitbot

--- src/bitbot.py
+++ src/bitbot.py
@@ -5,28 +5,31 @@ import wave
 import pyaudio
 import pygame
 import vlc
-
+import snowboydecoder
+# from omxplayer import OMXPlayer
 
 class Robot:
     def __init__(self):
         print("New Robot")
-        self.screen = pygame.display.set_mode((800, 480))
-        self.vlc = vlc.Instance("--no-xlib")
-        self.player = self.vlc.media_player_new()
-        self.player.set_xwindow(pygame.display.get_wm_info()["window"])
+        # self.screen = pygame.display.set_mode((800, 480))
+        # self.vlc = vlc.Instance("--no-xlib")
+        # self.player = self.vlc.media_player_new()
+        # self.player.set_xwindow(pygame.display.get_wm_info()["window"])
         self.emotion_thread = threading.Thread()
+        self.audio = pyaudio.PyAudio()
         # pygame.display.toggle_fullscreen()
 
     def _emotion(self, fname):
-        print(fname)
-        media = self.vlc.media_new_path("resources/emotions/"+fname+".mp4")
-        if self.player.is_playing:
-            self.player.stop()
-        self.player.set_media(media)
-        self.player.play()
-        while self.player.get_state() != vlc.State.Ended:
-            pass
-        self.player.stop()
+        pass
+        # print(fname)
+        # media = self.vlc.media_new_path("resources/emotions/"+fname+".mp4")
+        # if self.player.is_playing:
+        #     self.player.stop()
+        # self.player.set_media(media)
+        # self.player.play()
+        # while self.player.get_state() != vlc.State.Ended:
+        #     pass
+        # self.player.stop()
 
     def emotion(self, fname, wait=False):
         if self.emotion_thread.is_alive():
@@ -37,33 +40,49 @@ class Robot:
         self.emotion_thread = threading.Thread(target=self._emotion, args=(fname,))
         self.emotion_thread.start()
 
+    def _play_audio(self, fname):
+        ding_wav = wave.open(fname, 'rb')
+        ding_data = ding_wav.readframes(ding_wav.getnframes())
+        # audio = pyaudio.PyAudio()
+        stream_out = self.audio.open(
+            format=self.audio.get_format_from_width(ding_wav.getsampwidth()),
+            channels=ding_wav.getnchannels(),
+            rate=ding_wav.getframerate(), input=False, output=True)
+        stream_out.start_stream()
+        stream_out.write(ding_data)
+        time.sleep(0.2)
+        stream_out.stop_stream()
+        stream_out.close()
+        # audio.terminate()
+
     def speak(self, text):
         print("Speak:", text)
 
-    def play_wav(self, fname, wait=True):
+    # def play_wav(self, fname, wait=True):
 
-        def play():
-            audio = pyaudio.PyAudio()
-            fwav = wave.open(fname, 'rb')
-            fformat = audio.get_format_from_width(fwav.getsampwidth())
-            fchannels = fwav.getnchannels()
-            frate = fwav.getframerate()
-            fdata = fwav.readframes(fwav.getnframes())
-            print(fformat,fchannels,frate)
-            stream = audio.open(format=fformat,channels=fchannels,rate=frate, input=False, output=True)
-            stream.start_stream()
-            stream.write(fdata)
-            stream.stop_stream()
-            stream.close()
-            audio.terminate()
+    #     def play():
+    #         audio = pyaudio.PyAudio()
+    #         fwav = wave.open(fname, 'rb')
+    #         fformat = audio.get_format_from_width(fwav.getsampwidth())
+    #         fchannels = fwav.getnchannels()
+    #         frate = fwav.getframerate()
+    #         fdata = fwav.readframes(fwav.getnframes())
+    #         print(fformat,fchannels,frate)
+    #         stream = audio.open(format=fformat,channels=fchannels,rate=frate, input=False, output=True)
+    #         stream.start_stream()
+    #         stream.write(fdata)
+    #         stream.stop_stream()
+    #         stream.close()
+    #         audio.terminate()
 
-        if not wait:
-            threading.Thread(target=play).start()
-        else:
-            play()
+    #     if not wait:
+    #         threading.Thread(target=play).start()
+    #     else:
+    #         play()
 
  
-
+bitbot = Robot()
+bitbot._play_audio("resources/ding2.wav")
 
 # def main():
 #     bitbot = Robot()
--- src/runner.py
+++ src/runner.py
@@ -1,6 +1,16 @@
-from bitbot import *
+# from bitbot import *
 
-bb = Robot()
-for i in range(4):
-    bb.emotion("A-"+str(i+1),True)
-    time.sleep(1)
+# bb = Robot()
+# for i in range(4):
+#     bb.emotion("A-"+str(i+1),True)
+#     time.sleep(1)
+from omxplayer import OMXPlayer
+import time
+
+player = OMXPlayer("resources/emotions/A-1.mp4")
+player.play()
+
+time.sleep(5)
+player.pause()
+# player.
+player.quit()
