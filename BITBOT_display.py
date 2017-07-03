2dd0e7b4025f2b38e5d3eec5e7728b757514479e
Sirawat <oeyy.ey@gmail.com>
2017-07-03 18:53:41 +0700

    : 1.7 sec delay ,

--- BITBOT_display.py
+++ BITBOT_display.py
@@ -3,9 +3,12 @@ import argparse
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
 import time
+import tempfile
+import sys
 myprocess = 0
 myprocess2 = 0
-
+c = 0
+i = 0
 parser = argparse.ArgumentParser()
 parser.add_argument("DISPLAY_NUMBER")
 args = parser.parse_args()
@@ -15,10 +18,16 @@ if DISPLAY_NUMBER == '2':
 elif DISPLAY_NUMBER == '1':
     DISPLAY_NUMBER = '0'
 
+pdir = 'resources/emotions/'
+play_list = [pdir + 'A-1.mp4', pdir + 'A-2.mp4', pdir +
+             'A-3.mp4', pdir + 'A-4.mp4', pdir + 'A-5.mp4']
+
 
 def display(unused_addr, v1='../../small.mp4', v2=''):
-    global myprocess
-    cmd = ['omxplayer', '-b', '--display', DISPLAY_NUMBER, v1]
+    global myprocess, i
+    cmd = ['omxplayer', '--no-osd', '--layer',
+           str(i), '-b', '-o', 'alsa', '--display', DISPLAY_NUMBER, v1]
+    i += 1
     print('command : ', cmd)
     myprocess = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
@@ -31,11 +40,17 @@ def quit_video(unused_addr, v1):
 
 
 def play_new(unused_addr, v1):
-    global myprocess, myprocess2
+    global myprocess, myprocess2, c, i
     print('new playing')
-    cmd = ['omxplayer', '-b', '--display', DISPLAY_NUMBER, v1]
+    cmd = ['omxplayer', '--no-osd', '--layer', str(i), '--loop', '-b', '-o',
+           'alsa', '--display', DISPLAY_NUMBER, play_list[c]]
+    c += 1
+    i += 1
+    if c >= 5:
+        c = 0
     print('command : ', cmd)
     myprocess2 = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    time.sleep(1.7)
     print('exit old player')
     if myprocess != 0:
         myprocess.communicate(bytes('q', 'utf-8'))
@@ -48,7 +63,7 @@ def main():
     dispatcher.map("/q", quit_video)
     dispatcher.map("/n", play_new)
     server = osc_server.ThreadingOSCUDPServer(
-        ("192.168.1.244", 4000 + int(args.DISPLAY_NUMBER)), dispatcher)
+        ("192.168.1.243", 4000 + int(args.DISPLAY_NUMBER)), dispatcher)
     print("Display {} Serving on {}".format(args.DISPLAY_NUMBER, server.server_address))
     server.serve_forever()
 
