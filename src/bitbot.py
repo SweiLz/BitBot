f81ab540adca3a281c36692cf64e4010f4e27f04
Sirawat <oeyy.ey@gmail.com>
2017-07-06 14:06:40 +0700

    using class Robot

--- src/bitbot.py
+++ src/bitbot.py
@@ -130,10 +130,6 @@ class Robot:
 # bitbot.hdmi_close()
 
 
-
-
-# print(bitbot.checkHotword("resources/t.wav"))
-# print(bitbot.checkHotword("resources/t2.wav"))
 # print(bitbot.checkHotword("resources/t3.wav"))
 # def main():
 #     bitbot = Robot()
--- src/runner.py
+++
@@ -1,6 +0,0 @@
-from bitbot import *
-
-print("Hello")
-
-bitbot = Robot()
-bitbot.speak("สวัสดีทุกท่าน")
